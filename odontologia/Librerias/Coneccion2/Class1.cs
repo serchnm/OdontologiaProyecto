ffce80afbcac86332150cc0b469009aa28f6f51c
Eduardo1996-2018 <44241324+Eduardo1996-2018@users.noreply.github.com>
2018-10-26 14:40:27 -0700

    CAMBIOS MATERIAL

---
+++ odontologia/Librerias/Coneccion2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Coneccion2
+{
+    public class conectar
+    {
+        public static DataSet Ejecutar(string cmd)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Odonto;Integrated Security=True");
+            con.Open();
+            DataSet DS = new DataSet();
+
+            SqlDataAdapter dp = new SqlDataAdapter(cmd, con);
+            dp.Fill(DS);
+            con.Close();
+            return DS;
+
+
+
+        }
+    }
+}
