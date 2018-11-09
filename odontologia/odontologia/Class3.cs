e469ce86a4b9cadeefe5142d188201fca82186d5
serchnm <sergio.ortiz@tectijuana.edu.mx>
2018-11-09 15:28:43 -0800

    merch

---
+++ odontologia/odontologia/Class3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace odontologia
+{
+    class Class3
+    {
+
+
+
+        public static DataSet Ejecutar(string cmd)
+        {
+
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Odonto;Integrated Security=True");
+            con.Open();
+            DataSet DS = new DataSet();
+
+            SqlDataAdapter dp = new SqlDataAdapter(cmd, con);
+            dp.Fill(DS);
+            con.Close();
+            return DS;
+
+
+
+        }
+
+
+    }
+}
